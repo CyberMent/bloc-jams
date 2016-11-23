a925521a877818f78ca816024c38f64c1109f211
Henry Abel <henryabel@fastmail.com>
2016-11-23 01:36:17 -0500

    Checkpoint 23 Animations: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,9 +1,25 @@
-           var revealPoint = function (p) {
-               for (var i = 0; i < p.length; i++) {
-                   p[i].style.opacity = 1;
-                   p[i].style.transform = "scaleX(1) translateY(0)";
-                   p[i].style.msTransform = "scaleX(1) translateY(0)";
-                   p[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-               }
-           };
-           var thePoints = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
+var animatePoints = function (points) {
+    var revealPoint = function (index) {
+        points[index].style.opacity = 1;
+        points[index].style.transform = "scaleX(1) translateY(0)";
+        points[index].style.msTransform = "scaleX(1) translateY(0)";
+        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+    };
+    for (var i = 0; i < points.length; i++) {
+        revealPoint(i);
+    }
+};
+//animatePoints(pointsArray);
+window.onload = function () {
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    window.addEventListener('scroll', function (event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}
